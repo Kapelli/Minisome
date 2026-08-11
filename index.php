aa8750c2dd30cdad29c6d2e00ca2e6ecacac4405
Kaapo Kivijarvi <kaapo.kivijarvi@samiedu365.fi>
2026-08-11 12:38:14 +0300

    Luotu index.php yhteydden muodostus ja sql kyselyt näkyy sivustolla

--- index.php
+++ index.php
@@ -2,6 +2,22 @@
 
 include 'database.php';
 
-echo "Tietokantayhteys muodostettu onnistuneesti.";
+echo "Tietokantayhteys muodostettu onnistuneesti." . "<br>";
+
+
+
+// SQL-kysely, joka hakee kaikki julkaisut
+$sql = "SELECT * FROM posts";
+
+// Suoritetaan kysely
+$result = mysqli_query($conn, $sql);
+
+// Käsitellään kyselyn palauttamat rivit
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['author'] . "<br>";
+    echo $row['content'] . "<br>";
+    echo $row['created_at'] . "<br>";
+    echo "<hr>";
+}
 
 ?>

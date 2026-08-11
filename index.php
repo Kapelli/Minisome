3e9ddd3bdab19d2d0990cb927d0d658ac3d98387
Kaapo Kivijarvi <kaapo.kivijarvi@samiedu365.fi>
2026-08-11 13:44:36 +0300

    Lisätty CSS

--- index.php
+++ index.php
@@ -1,23 +1,50 @@
+
 <?php
 
 include 'database.php';
 
 echo "Tietokantayhteys muodostettu onnistuneesti." . "<br>";
 
-
-
-// SQL-kysely, joka hakee kaikki julkaisut
 $sql = "SELECT * FROM posts";
-
-// Suoritetaan kysely
 $result = mysqli_query($conn, $sql);
 
-// Käsitellään kyselyn palauttamat rivit
-while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['author'] . "<br>";
-    echo $row['content'] . "<br>";
-    echo $row['created_at'] . "<br>";
-    echo "<hr>";
-}
-
 ?>
+
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minisome</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="postaukset">
+
+    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+
+        <div class="post">
+
+            <div class="tekija">
+                <?php echo $row['author']; ?>
+            </div>
+
+            <div class="sisalto">
+                <?php echo $row['content']; ?>
+            </div>
+
+            <div class="julkaisuaika">
+                <?php echo $row['created_at']; ?>
+            </div>
+
+        </div>
+
+    <?php } ?>
+
+</div>
+
+</body>
+</html>
+
